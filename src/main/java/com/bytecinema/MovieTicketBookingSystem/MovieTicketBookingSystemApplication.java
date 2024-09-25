e765444287f536f5da66013c983fd0b837b959ee
QuyHoang7703 <hoangvanquy772003@gmail.com>
2024-09-25 19:34:47 +0700

    config basic project

---
+++ src/main/java/com/bytecinema/MovieTicketBookingSystem/MovieTicketBookingSystemApplication.java
@@ -0,0 +1,19 @@
+package com.bytecinema.MovieTicketBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//disable security
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
+
+// @SpringBootApplication
+public class MovieTicketBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketBookingSystemApplication.class, args);
+	}
+
+}
