4eaaaf3aaa5c1ccddc6ca633283559c1e401bdcf
QuyHoang7703 <hoangvanquy772003@gmail.com>
2024-09-25 21:28:17 +0700

    create some api for users

--- src/main/java/com/bytecinema/MovieTicketBookingSystem/controller/UserController.java
+++ src/main/java/com/bytecinema/MovieTicketBookingSystem/controller/UserController.java
@@ -3,7 +3,9 @@ package com.bytecinema.MovieTicketBookingSystem.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +31,18 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(this.userService.handleCreateUser(user));
     }
 
+    @GetMapping("/users/{id}")
+    @ApiMessage("Fetch user by ID")
+    public ResponseEntity<User> fecthUserByID(@PathVariable("id") long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.userService.fetchUserById(id));
+    }
 
+    @DeleteMapping("/users/{id}")
+    @ApiMessage("Delete a user")
+    public ResponseEntity<Void> deleteUser(@PathVariable("id") long id) {
+        this.userService.handleDeleteUser(id);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
 
 
 
--- src/main/java/com/bytecinema/MovieTicketBookingSystem/service/UserService.java
+++ src/main/java/com/bytecinema/MovieTicketBookingSystem/service/UserService.java
@@ -29,6 +29,10 @@ public class UserService {
         return this.userRepository.findByEmail(email);
     }
 
+    public void handleDeleteUser(long id) {
+        this.userRepository.deleteById(id);
+    }
+
     
     
 }
