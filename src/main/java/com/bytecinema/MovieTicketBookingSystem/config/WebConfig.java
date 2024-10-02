9a4e1f01a26826c22501badff16a555d6067b815
QuyHoang7703 <hoangvanquy772003@gmail.com>
2024-10-03 00:26:20 +0700

    disable cors

--- src/main/java/com/bytecinema/MovieTicketBookingSystem/config/WebConfig.java
+++ src/main/java/com/bytecinema/MovieTicketBookingSystem/config/WebConfig.java
@@ -8,12 +8,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    // @Override
+    // public void addCorsMappings(CorsRegistry registry) {
+    //     registry.addMapping("/**")  // Cho phép tất cả các endpoint
+    //             .allowedOrigins("http://localhost:5173")  // Thay thế bằng domain của frontend
+    //             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Các phương thức được phép
+    //             .allowedHeaders("*")  // Cho phép tất cả các header
+    //             .allowCredentials(true);  // Cho phép gửi cookie nếu cần
+    // }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // Cho phép tất cả các endpoint
-                .allowedOrigins("http://localhost:5173")  // Thay thế bằng domain của frontend
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Các phương thức được phép
-                .allowedHeaders("*")  // Cho phép tất cả các header
-                .allowCredentials(true);  // Cho phép gửi cookie nếu cần
+        registry.addMapping("/**") // Áp dụng cho tất cả các endpoint
+                .allowedOrigins("*") // Cho phép tất cả các domain
+                .allowedMethods("*") // Cho phép tất cả các phương thức
+                .allowedHeaders("*") // Cho phép tất cả các header
+                .allowCredentials(true); // Cho phép gửi cookie
     }
 }
